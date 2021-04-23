1b797608a853982ee5cdd77821fa867075ed53b4
rawskiWSB <rawski.piotr@yahoo.pl>
2021-04-23 14:21:15 +0200

    env var

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Parser = require('rss-parser')
 const mjml2html = require('mjml')
 // const API_KEY = "";
 // const DOMAIN = "";
-const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+const mailgun = require('mailgun-js')({apiKey: process.env.API_KEY, domain: process.env.DOMAIN});
 
 app.use("/static", express.static("public"));
 
